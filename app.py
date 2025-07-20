48aa2024b2ce8d2528dc2c7900142e0758a634ce
MrCastillooo <nanaxo1213@gmail.com>
2025-07-20 20:57:48 -0300

    secret key modificacion

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ if db_url.startswith("postgres://"):
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 
-app.config['SECRET_KEY'] = 'la-clave-mas-segura-del-universo-final'
+# Lee la clave secreta de forma segura desde las variables de entorno
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
